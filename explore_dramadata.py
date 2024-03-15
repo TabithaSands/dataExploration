36bc622763ceeb484bf03fcd29bea60a2d249848
tabitha <Tabitha.sands11@gmail.com>
2024-03-14 22:28:44 -0500

    Date exploration

--- explore_dramadata.py
+++ explore_dramadata.py
@@ -19,4 +19,11 @@ df_scored = df_trimmed[df_trimmed['tot_user_score'] == df_trimmed['tot_user_scor
 df_tot_Watched = df_trimmed[df_trimmed['tot_watched'] == df_trimmed['tot_watched'].max()]
 a = pd.concat([df_scored, df_tot_Watched])
 # add com
-df_comedy2 = df_trimmed[df_trimmed["genres"].str.contains('Comedy', na = False)]
+df_comedy2 = df_trimmed[df_trimmed["genres"].str.contains('Comedy', na=False)]
+# exploring date and time
+df_duration = pd.read_csv("/Users/tabitha/PycharmProjects/dataExploration/data/asian_drama_data/kor_drama.csv")
+# Converted str date column to datetime datatype
+df_duration['start_dt'] = pd.to_datetime(df_duration['start_dt'], format='mixed')
+df_duration['end_dt'] = pd.to_datetime(df_duration['end_dt'], format='mixed')
+# created new column duration in df
+df_duration["duration"] = df_duration["end_dt"] - df_duration["start_dt"]
